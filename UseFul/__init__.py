9df8fbe6007e56011a7e4c801cb98fa4708e1327
mav <dot1mav>
2021-08-23 20:18:17 +0430

    add logger and imports

--- UseFul/__init__.py
+++ UseFul/__init__.py
@@ -1,4 +1,12 @@
+__all__ = ["StoppableThread", "Thread_", "TimerThread", "thread_function",
+           "func_log","FLOGGER"
+           "DatabaseManager"]
 __doc__ = """"""
 
-from .threading import StoppableThread, _Thread, TimerThread, thread_function
+from logbook import Logger
+
+FLOGGER = Logger("FUNCTION")
+
+from .threading import StoppableThread, Thread_, TimerThread, thread_function
+from .functions import func_log
 from .pyDb import DatabaseManager
